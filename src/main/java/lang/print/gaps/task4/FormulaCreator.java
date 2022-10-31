3c3e10cb53e2e97cf638d7b42cfeb63c1c0ece7a
wilkat <katarzyna.f.wilk@gmail.com>
2022-10-31 22:50:54 +0100

    new

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a;
+        int b;
+        int formula = (9*Math.pow(a,2) - 5*b + 2 + a - 7)*((a + b - 4*a*b)/2);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        char c = 99;
+        char h = 104;
+        char a = 97;
+        char r = 114;
     }
 }
