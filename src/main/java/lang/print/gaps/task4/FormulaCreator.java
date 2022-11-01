7cfe79242dab2dc853443519fd2f2ec5eb1b0121
wilkat <katarzyna.f.wilk@gmail.com>
2022-11-01 09:37:44 +0100

    new

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,8 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        int a;
-        int b;
+        double a = 1;
+        double b = 1;
         double formula = (9*Math.pow(a,2) - 5*b + 2 + a - 7)*((a + b - 4*a*b)/2);
         System.out.println(formula);
     }
